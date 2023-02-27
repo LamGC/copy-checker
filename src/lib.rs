97f140cc263fb685b8fb8f84b6c61158ecd47e34
LamGC <lam827@lamgc.net>
2023-02-27 20:20:09 +0800

    refactor: 将实际功能移到 lib.rs 文件.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::{fs::{OpenOptions, File}, io::Write, fmt::{self, Display}};
+use std::{fs::{OpenOptions, File}, io::{Write, stdout, Read}, fmt::{self, Display}, path::Path};
+
+use sha2::{Sha256, Digest};
+use walkdir::WalkDir;
 
 pub enum FileCheckResult {
     OK,
@@ -63,5 +66,84 @@ impl FileCheckRecorder {
 
 }
 
+pub fn copy_check(source_root_path: &Path, dest_root_path: &Path, recorder: &mut FileCheckRecorder) {
+    let walkdir = WalkDir::new(source_root_path)
+        .sort_by_file_name()
+        .follow_links(true)
+        .into_iter();
+
+    println!("Scanning source path...");
+    for entry in walkdir {
+        match entry {
+            Ok(entry) => {
+                if !entry.path().is_file() {
+                    continue;
+                }
+                let r_path = pathdiff::diff_paths(entry.path(), source_root_path)
+                    .expect("Cannot get relative path.");
+                print!("Checking `{}`......", r_path.display());
+                stdout().flush().expect("Cannot flush stdout.");
+                let dest_file_path = dest_root_path.join(&r_path);
+                if !Path::exists(&dest_file_path) {
+                    println!("Not exists!");
+                    recorder.fail(
+                        &r_path.to_str().expect("Cannot get path str."),
+                        FileCheckResult::NotFound,
+                    );
+                    continue;
+                }
+
+                let source_digest =
+                    get_file_digest(entry.path()).expect("Failed to calculate source file digest.");
+                let dest_digest = get_file_digest(&dest_file_path)
+                    .expect("Failed to calculate destination file digest.");
+
+                let result = if source_digest == dest_digest {
+                    println!("OK!");
+                    FileCheckResult::OK
+                } else {
+                    println!("Not equal!");
+                    FileCheckResult::NoEqual
+                };
+                stdout().flush().expect("Cannot flush stdout.");
+                
+                let source_digest = hex::encode(source_digest);
+                let dest_digest = hex::encode(dest_digest);
+
+                let r_path = &r_path.to_str().expect("Cannot get path str.");
+
+                recorder.append(
+                    &r_path,
+                    result,
+                    source_digest,
+                    dest_digest,
+                );
+            }
+            Err(e) => {
+                eprintln!("Cannot access file: {e}");
+            }
+        }
+    }
+    println!("Checks complete!");
+}
 
 
+fn get_file_digest(path: &Path) -> Result<[u8; 32], std::io::Error> {
+    let mut file = File::open(path)?;
+    const BUFFER_SIZE: usize = 1024 * 100; // 10K
+    let mut buffer = [0; BUFFER_SIZE];
+    let mut digest = Sha256::new();
+    loop {
+        let read_size = file.read(&mut buffer)?;
+        if read_size == 0 {
+            break;
+        }
+        digest.update(&buffer[..read_size]);
+        if read_size != BUFFER_SIZE {
+            break;
+        }
+    }
+    let mut digest_result: [u8; 32] = [0; 32];
+    digest_result.copy_from_slice(digest.finalize().as_slice());
+    Ok(digest_result)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::{env, fs::File, io::Read, path::Path, process, io::stdout, io::Write};
+use std::{env, path::Path, process};
 
-use copy_checker::{FileCheckRecorder, FileCheckResult};
-use hex;
-use pathdiff;
-use sha2::{Digest, Sha256};
-use walkdir::WalkDir;
+use copy_checker::{FileCheckRecorder, copy_check};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -40,82 +36,8 @@ fn main() {
 
     let mut recorder = FileCheckRecorder::new(record_file_path)
         .expect(&format!("Cannot open record file: {record_file_path}")[..]);
-    let walkdir = WalkDir::new(source_root_path)
-        .sort_by_file_name()
-        .follow_links(true)
-        .into_iter();
 
-    println!("Scanning source path...");
-    for entry in walkdir {
-        match entry {
-            Ok(entry) => {
-                if !entry.path().is_file() {
-                    continue;
-                }
-                let r_path = pathdiff::diff_paths(entry.path(), source_root_path)
-                    .expect("Cannot get relative path.");
-                print!("Checking `{}`......", r_path.display());
-                stdout().flush().expect("Cannot flush stdout.");
-                let dest_file_path = dest_root_path.join(&r_path);
-                if !Path::exists(&dest_file_path) {
-                    println!("Not exists!");
-                    recorder.fail(
-                        &r_path.to_str().expect("Cannot get path str."),
-                        FileCheckResult::NotFound,
-                    );
-                    continue;
-                }
-
-                let source_digest =
-                    get_file_digest(entry.path()).expect("Failed to calculate source file digest.");
-                let dest_digest = get_file_digest(&dest_file_path)
-                    .expect("Failed to calculate destination file digest.");
-
-                let result = if source_digest == dest_digest {
-                    println!("OK!");
-                    FileCheckResult::OK
-                } else {
-                    println!("Not equal!");
-                    FileCheckResult::NoEqual
-                };
-                stdout().flush().expect("Cannot flush stdout.");
-                
-                let source_digest = hex::encode(source_digest);
-                let dest_digest = hex::encode(dest_digest);
-
-                let r_path = &r_path.to_str().expect("Cannot get path str.");
-
-                recorder.append(
-                    &r_path,
-                    result,
-                    source_digest,
-                    dest_digest,
-                );
-            }
-            Err(e) => {
-                eprintln!("Cannot access file: {e}");
-            }
-        }
-    }
-    println!("Checks complete!");
+    copy_check(source_root_path, dest_root_path, &mut recorder);
+    
 }
 
-fn get_file_digest(path: &Path) -> Result<[u8; 32], std::io::Error> {
-    let mut file = File::open(path)?;
-    const BUFFER_SIZE: usize = 1024 * 100; // 10K
-    let mut buffer = [0; BUFFER_SIZE];
-    let mut digest = Sha256::new();
-    loop {
-        let read_size = file.read(&mut buffer)?;
-        if read_size == 0 {
-            break;
-        }
-        digest.update(&buffer[..read_size]);
-        if read_size != BUFFER_SIZE {
-            break;
-        }
-    }
-    let mut digest_result: [u8; 32] = [0; 32];
-    digest_result.copy_from_slice(digest.finalize().as_slice());
-    Ok(digest_result)
-}
